8f9ce9f82f53f4c287e3ed750abd9529da444db6
Alina Yagnaeva <as.yagnaeva97@gmail.com>
2023-11-01 17:55:41 +0300

    hw3

---
+++ HW3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+if (number == "1" || number == "2" || number == "3" || number == "4" || number == "5")
+{
+    Console.WriteLine($"{number} -> не выходной");
+}
+else if (number == "6" || number == "7")
+{
+    Console.WriteLine($"{number} -> выходной день");
+}
+else
+{
+    Console.WriteLine("Нет такого дня");
+}
