657b6f22ea081ad0dd11bea27a741a07403573e3
Alina Yagnaeva <as.yagnaeva97@gmail.com>
2023-11-01 17:55:02 +0300

    hw2

---
+++ HW2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+string stringNumber = Console.ReadLine();
+
+int number;
+bool isNumber = int.TryParse(stringNumber, out number);
+
+if (isNumber && stringNumber.Length >=3)
+{
+    Console.WriteLine($"{stringNumber} -> {stringNumber[2]}");
+}
+else if (isNumber && stringNumber.Length < 3)
+{
+    Console.WriteLine($"В числе {stringNumber} нет третьей цифры");
+}
+else
+{
+    Console.WriteLine($"Вы ввели не число");
+}
