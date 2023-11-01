823f2c6d0b5bbc5604050aa5c049c5fdbc70daff
Alina Yagnaeva <as.yagnaeva97@gmail.com>
2023-11-01 17:54:26 +0300

    hw1

---
+++ HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число: ");
+string stringNumber = Console.ReadLine();
+
+int number;
+bool isNumber = int.TryParse(stringNumber, out number);
+
+if (isNumber && stringNumber.Length == 3)
+{
+    Console.WriteLine($"{stringNumber} -> {stringNumber[1]}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число, либо там есть буквы.");
+}
